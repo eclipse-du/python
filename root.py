29b59c6bfbd0504de5263d2deed0969991346c70
eclipse-du <adooadoo@163.com>
2013-05-06 23:26:38 +0800

    Fivth Trial

--- root.py
+++ root.py
@@ -9,9 +9,12 @@ def root(x,iterMax = 5):
     """
     s=1.0
     b=1
+    s1 = s
+    tol = 1.e-14
     for k in range(iterMax):
         s = 0.5 * (s + x/s)
+        if abs((s-s1)/x) < tol:
+            return s
     return s
 
 print root(2.)
-print root(2.,1)
