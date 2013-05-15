fd7c3dc75d36b978c073d6dafc80ab53fe28410d
eclipse-du <adooadoo@163.com>
2013-05-16 00:42:43 +0800

    make some debug

--- root.py
+++ root.py
@@ -4,17 +4,18 @@
 # email   : adooadoo@163.com
 # filename: root.py
 
-def root(x,iterMax = 5):
+def root(x,debug = False,iterMax = 100):
     """
     """
     s=1.0
-    b=1
-    s1 = s
     tol = 1.e-14
     for k in range(iterMax):
+        s1 = s
         s = 0.5 * (s + x/s)
+        if debug:
+            print "Before iteration %s,s=%20.15f" %(k,s)
         if abs((s-s1)/x) < tol:
             return s
     return s
 
-print root(2.)
+print root(2.,True)
