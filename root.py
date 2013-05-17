2b31cd94944f0f8f7c8af44007fbba19d483b460
eclipse-du <adooadoo@163.com>
2013-05-17 12:38:23 +0800

    test function added

--- root.py
+++ root.py
@@ -1,12 +1,19 @@
-#!/usr/bin/python
-#-*- coding:utf-8 -*-
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 # author  : eclipse
 # email   : adooadoo@163.com
-# filename: root.py
+
 
 def root(x,debug = False,iterMax = 100):
     """
+    root for none-negative number
     """
+    if x==0:
+        return 0
+    elif x<0:
+        print "***Erro,x must be nonnegative"
+        return None
+    assert x>0. and type(x) is float or int ,"Unrecognized input"
     s=1.0
     tol = 1.e-14
     for k in range(iterMax):
@@ -18,4 +25,17 @@ def root(x,debug = False,iterMax = 100):
             return s
     return s
 
-print root(2.,True)
+def test():
+    from numpy import sqrt
+    xvalues = [0.,2.,100.,10000.,1.e-4]
+    for x in xvalues:
+        print "Testing with x = %20.15e" % x
+        s = root(x)
+        s_numpy = sqrt(x)
+        print " s = %20.15e, numpy.sqrt = %20.15e" \
+              % (s,s_numpy)
+        assert abs(s - s_numpy)<1e-14,\
+               "Disagree for x = %20.15e" % x
+if __name__ == "__main__":
+    print "Running test."
+    test()
